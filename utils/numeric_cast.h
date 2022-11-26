452405b0882b293df1eeb7b9ec3a322b73e6f199
Hulevych Mykhailo <mykhailo.hulevych@collabio.team>
2022-11-27 00:48:13 +0200

    headers only boost dependency has been added

--- utils/numeric_cast.h
+++ utils/numeric_cast.h
@@ -1,26 +1,39 @@
 #pragma once
 
 #include <exception>
+#include <type_traits>
+
+#include <boost/numeric/conversion/cast.hpp>
 
 namespace gunit {
 class BadNumCast final : public std::exception {
  public:
-  const char* what() const _NOEXCEPT override { return "bad num_cast"; }
+  explicit BadNumCast(const char* what) : _error("BadNumCast: ") {
+    _error += what;
+  }
+
+  const char* what() const _NOEXCEPT override { return _error.c_str(); }
+
+ private:
+  std::string _error;
 };
 
 template <typename To,
           typename From,
           std::enable_if_t<std::is_same<To, From>::value>* = nullptr>
-To numCast(From from) {
+To numCast(const From from) {
   return from;
 }
 
 template <typename To,
           typename From,
           std::enable_if_t<!std::is_same<To, From>::value>* = nullptr>
-To numCast(From from) {
-  // TODO: implement safe cast;
-  return static_cast<To>(from);
+To numCast(const From from) {
+  try {
+    return boost::numeric_cast<To>(from);
+  } catch (const boost::bad_numeric_cast& exception) {
+    throw BadNumCast(exception.what());
+  }
 }
 
 }  // namespace gunit
